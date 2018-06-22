8974dbdbebfa65bda2417488e043e33e9b392b62
magentall <magentallteller@gmail.com>
2018-06-22 16:09:12 +0200

    easy22.1

--- api/tabdispocat.php
+++ api/tabdispocat.php
@@ -13,7 +13,9 @@ if (isset($_POST)&& !empty($_POST)){
     //$result = req("SELECT jeux.num_jeu FROM jeux LEFT JOIN prets ON jeux.num_jeu = prets.num_jeu WHERE prets.num_jeu IS NULL;");
     //INNER JOIN categorie ON jeux.nom_categorie=categorie.nom_categorie
 
-    $result = req("SELECT jeux.num_jeu,jeux.ref_jeu,nom_jeu,code_cat FROM jeux as jeux LEFT JOIN prets ON jeux.num_jeu = prets.num_jeu WHERE (prets.num_jeu IS NULL AND jeux.code_cat='$categorie') OR ('$today' >= prets.date_retour AND jeux.code_cat='$categorie') ORDER BY num_jeu DESC");
+    //$result = req("SELECT jeux.num_jeu,jeux.ref_jeu,nom_jeu,code_cat FROM jeux as jeux LEFT JOIN prets ON jeux.num_jeu = prets.num_jeu WHERE (prets.num_jeu IS NULL AND jeux.code_cat='$categorie') OR ('$today' >= prets.date_retour AND jeux.code_cat='$categorie') ORDER BY num_jeu DESC");
+    $result = req("SELECT jeux.num_jeu,jeux.ref_jeu,nom_jeu,code_cat FROM jeux as jeux LEFT JOIN prets ON jeux.num_jeu = prets.num_jeu WHERE (prets.num_jeu IS NULL AND jeux.code_cat='$categorie' AND (jeux.code='' OR jeux.code=1)) OR ('$today' >= prets.date_retour AND jeux.code_cat='$categorie' AND (jeux.code='' OR jeux.code=1)) ORDER BY num_jeu DESC");
+
 
     $outp = res2json_only($result);
 
