a822f582b657f359c08c4cabe89c8c7f9d3694ec
magentall <magentallteller@gmail.com>
2018-06-28 15:54:53 +0200

    eaasy28.6.5

--- api/addpret.php
+++ api/addpret.php
@@ -18,15 +18,15 @@ if (isset($_POST)&& !empty($_POST)){
     $date_pret=$today;
   }
 
-  if ($date_retour<=$today) {
+  /*if ($date_retour<=$today) {
     $date_retour=date('Y-m-d', strtotime($today. ' + 21 days'));
   }
 
-  if ($date_pret<=$date_retour) {
+  if ($date_retour<=$date_retour) {
     $date_pret=$today;
     $date_retour=date('Y-m-d', strtotime($today. ' + 21 days'));
   }
-
+*/
   $sql = "INSERT INTO prets (num_jeu, num_adherent, date_pret, date_retour, prix) VALUES ('$numjeu', '$numadh', '$date_pret', '$date_retour', '$prix')";
 
 
